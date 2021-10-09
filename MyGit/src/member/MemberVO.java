e687ba0014e0958883b3f240d5387159cd5fcd49
JuYeonson <jubar22222@gmail.com>
2021-10-09 10:43:35 +0900

    2번째 충돌실험

--- MyGit/src/member/MemberVO.java
+++ MyGit/src/member/MemberVO.java
@@ -3,8 +3,9 @@ package member;
 import java.util.Calendar;
 
 public class MemberVO {
-
-	   String s1 = "원래 7번라인부터 시작했지만 실험을 위해 수정 ";
+	   
+	   String s2 = "2번째 충돌실험 ";
+	   String s1 = "원래 7번라인부터 시작했지만 실험을 위해 수정 (원래 7번 라인에 있었음)";
 	   private String userid;             // 회원아이디
 	   private String pwd;                // 비밀번호 (SHA-256 암호화 대상)
 	   private String name;               // 회원명
